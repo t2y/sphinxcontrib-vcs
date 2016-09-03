8a0b8526132ebcd8defb5219b590147d6fa3ff05
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2016-09-03 15:23:21 +0900

    add initial sphinx extension for git repository

---
+++ sphinxcontrib/repository/_git.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+import re
+
+from git import Repo
+
+
+HOSTING_SERVICE = {
+    'github': {
+        'site': 'https://github.com',
+        'commit_template': '{site}/{user}/{repository}/commit/{sha}',
+    },
+    'bitbucket': {
+        'site': 'https://bitbucket.org',
+        'commit_template': '{site}/{user}/{repository}/commits/{sha}',
+    },
+}
+
+HOSTING_INFO_PATTERN = re.compile(
+    r'git@.*?:(?P<account>.*?)/(?P<repository_name>.*?)\.git',
+)
+
+
+class GitRepository(Repo):
+
+    EMPTY_TREE_SHA = '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._hexsha = {}
+        self._commits = []
+
+    @property
+    def branch_name(self):
+        return self.head.ref.name
+
+    def get_commits(self, max_count=None, **kwargs):
+        if len(self._commits) == 0 or max_count is not None:
+            self._commits[:] = []
+            if max_count is None:
+                max_count = 50
+
+            g = enumerate(self.iter_commits(max_count=max_count, **kwargs))
+            for index, commit in g:
+                self._hexsha[commit.hexsha] = index
+                self._commits.append(commit)
+
+        return self._commits
+
+    def get_diff(self, revision):
+        if len(self._commits) == 0:
+            self.get_commits()
+
+        index = self._hexsha[revision]
+        target_commit = self._commits[index]
+        if target_commit.parents:
+            prev_hexsha = self._commits[index + 1].hexsha
+        else:
+            prev_hexsha = self.EMPTY_TREE_SHA
+        return self.git.diff(prev_hexsha, target_commit.hexsha)
+
+    def make_commit_url(self, site, revision):
+        m = re.match(HOSTING_INFO_PATTERN, self.remotes.origin.url)
+        if m is None:
+            return ''
+
+        info = m.groupdict()
+        return site['commit_template'].format(
+            site=site['site'],
+            user=info['account'],
+            repository=info['repository_name'],
+            sha=revision,
+        )
+
+    def get_commit_url(self, revision):
+        if len(self._commits) == 0:
+            self.get_commits()
+
+        site = find_hosting_site(self.remotes.origin.url)
+        if site is None:
+            return ''
+
+        return self.make_commit_url(site, revision)
+
+
+def get_repo(path):
+    return GitRepository(path, search_parent_directories=True)
+
+
+def find_hosting_site(url):
+    if url.find('github.com') > 0:
+        return HOSTING_SERVICE['github']
+    elif url.find('bitbucket.org') > 0:
+        return HOSTING_SERVICE['bitbucket']
+    return None
+
+
+def test():
+    r = get_repo('.')
+
+    commits = r.get_commits(max_count=3)
+    print(commits)
+    last_diff = r.get_diff(commits[0].hexsha)
+    print(last_diff)
+    commit_url = r.get_commit_url(commits[0].hexsha)
+    print(commit_url)
+
+
+if __name__ == '__main__':
+    test()
---
+++ sphinxcontrib/vcs.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from docutils import nodes
+from docutils.parsers.rst import Directive
+
+from .repository import GitRepository
+
+__version__ = '0.1.0'
+
+
+class BaseDirective(Directive):
+
+    TARGET_ID = 'vcs'
+
+    option_spec = {
+        'include_diff': bool,
+        'with_ref_url': bool,
+    }
+
+    def run(self):
+        list_node = nodes.bullet_list()
+        env = self.state.document.settings.env
+        repo = GitRepository(env.srcdir)
+
+        targetnode = nodes.target('', '', ids=[self.TARGET_ID])
+        for commit in repo.get_commits():
+            item = self.get_changelog(repo, commit)
+            list_node.append(item)
+        return targetnode + [list_node]
+
+
+class GitDirective(BaseDirective):
+
+    def get_changelog(self, repo, commit):
+        item = nodes.list_item()
+
+        commit_date = datetime.fromtimestamp(commit.authored_date)
+
+        item.append(nodes.strong(text=commit.message))
+        item.append(nodes.inline(text=' by '))
+        item.append(nodes.emphasis(text=commit.author.name))
+        item.append(nodes.inline(text=' at '))
+        item.append(nodes.emphasis(text=str(commit_date)))
+
+        if self.options.get('with_ref_url') is not None:
+            ref_url = repo.get_commit_url(commit.hexsha)
+            ref = nodes.reference('', commit.hexsha, refuri=ref_url)
+            item.append(nodes.paragraph('', '', ref))
+
+        if self.options.get('include_diff') is not None:
+            diff = repo.get_diff(commit.hexsha)
+            item.append(nodes.literal_block(text=diff))
+
+        return item
+
+
+def setup(app):
+    app.add_directive('git', GitDirective)
+
+    return {
+        'version': __version__,
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
