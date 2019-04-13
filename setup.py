d2b208a5576d1012ff0de9ae1b3bb674ea62181f
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2019-04-13 12:04:30 +0900

    update tox test environment and drop 3.4 support

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
